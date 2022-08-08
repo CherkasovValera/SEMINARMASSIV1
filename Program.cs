4bce222ccc00953175a770c12b305a967880b3dc
Valeriy <cherkasov77@mail.ru>
2022-08-09 00:57:25 +0400

    Нахождение максимума из двух чисел

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+Console.WriteLine("Введите 1 число:  ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите 2 число:  ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+int max = number1;
+if ( number2 < max)
+    Console.WriteLine( $"Максимальное значение =  {number1}");
+        else
+   {
+         Console.WriteLine( $"Максимальное значение =  {number2}");
+   }
