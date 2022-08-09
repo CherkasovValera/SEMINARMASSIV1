0151a0413561c8688836edad1131c89c57c916e2
Valeriy <cherkasov77@mail.ru>
2022-08-09 22:42:40 +0400

    Задача Номер2 Финал

--- Program.cs
+++ Program.cs
@@ -1,17 +1,35 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+﻿// Задача №1 Напишите программу, которая на вход принимает два числа и выдаёт, 
 // какое число большее, а какое меньшее.
-Console.WriteLine("Введите 1 число:  ");
-int number1 = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine("Введите 2 число:  ");
-int number2 = Convert.ToInt32 (Console.ReadLine());
-int max = number1;
-if (max == number2)
+//Console.WriteLine("Введите 1 число:  ");
+//int number1 = Convert.ToInt32 (Console.ReadLine());
+//Console.WriteLine("Введите 2 число:  ");
+//int number2 = Convert.ToInt32 (Console.ReadLine());
+//int max = number1;
+//if (max == number2)
 
-Console.WriteLine( $"Значения равны =  {number1}");
-    else
-    if ( number2 < max)
-            Console.WriteLine( $"Максимум =  {number1}; Минимум = {number2}");
-         else
-     {
-            Console.WriteLine( $"Максимум =  {number2}; Минимум = {number1}");
-     }
+//Console.WriteLine( $"Значения равны =  {number1}");
+// else
+//if ( number2 < max)
+//      Console.WriteLine( $"Максимум =  {number1}; Минимум = {number2}");
+//  else
+// {
+//       Console.WriteLine( $"Максимум =  {number2}; Минимум = {number1}");
+// }
+
+
+
+// зАДАЧА №2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите 1 число:  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число:  ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число:  ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+{
+    Console.WriteLine($"Максимум =  {max}");
+}
